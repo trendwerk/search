b178ad1dbea5b4b12491d18f14d045216be2b559
Harold Angenent <harold@trendwerk.nl>
2017-02-13 15:31:19 +0100

    ✅ Sample test

---
+++ tests/Search/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Trendwerk\Search\Test;
+
+use WP_UnitTestCase;
+
+final class SampleTest extends WP_UnitTestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+}
