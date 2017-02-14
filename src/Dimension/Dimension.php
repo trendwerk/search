455de6c35f8b580b0b1070b489df7a6738313e05
Harold Angenent <harold@trendwerk.nl>
2017-02-14 16:30:18 +0100

    🚧 Dimension->search() for Meta

--- src/Dimension/Dimension.php
+++ src/Dimension/Dimension.php
@@ -5,5 +5,7 @@ use wpdb;
 
 interface Dimension
 {
+    public function __construct(array $options);
     public function join(wpdb $wpdb);
+    public function search(wpdb $wpdb, array $words);
 }
--- src/Dimension/Meta.php
+++ src/Dimension/Meta.php
@@ -1,15 +1,32 @@
 <?php
 namespace Trendwerk\Search\Dimension;
 
+use BadMethodCallException;
 use wpdb;
 
 final class Meta implements Dimension
 {
+    private $options;
     private $tableAlias = 'searchMeta';
+
+    public function __construct(array $options)
+    {
+        if (! isset($options['key'])) {
+            throw new BadMethodCallException('`key` is a required property.');
+        }
+
+        $this->options = $options;
+    }
 
     public function join(wpdb $wpdb)
     {
         return 'INNER JOIN ' . $wpdb->postmeta . ' AS ' . $this->tableAlias .
         ' ON (' . $wpdb->posts . '.ID = ' . $this->tableAlias . '.post_id)';
+    }
+
+    public function search(wpdb $wpdb, array $words)
+    {
+        return '(' . $this->tableAlias . '.meta_key = \'' . $this->options['key'] . '\' AND ' .
+        $this->tableAlias . '.meta_value REGEXP \'' . implode('|', $words) . '\')';
     }
 }
