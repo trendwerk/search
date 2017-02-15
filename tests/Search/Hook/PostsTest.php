53a01b1570f2441011e85ed8ea8f48169ec1ef47
Harold Angenent <harold@trendwerk.nl>
2017-02-15 14:03:11 +0100

    ✅ Posts init

---
+++ tests/Search/Hook/PostsTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Trendwerk\Search\Test;
+
+use Mockery;
+use Trendwerk\Search\Dimension\Dimensions;
+use Trendwerk\Search\Dimension\Meta;
+use Trendwerk\Search\Hook\Posts;
+use WP_Mock;
+
+final class PostsTest extends TestCase
+{
+    private $posts;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $wpdb = Mockery::mock('wpdb');
+
+        $dimensions = new Dimensions();
+        $dimensions->add(new Meta($wpdb, [
+            'key' => 'lastName',
+        ]));
+        
+        $this->posts = new Posts($dimensions);
+    }
+
+    public function testInit()
+    {
+        $argsCount = 2;
+        $priority = 10;
+
+        WP_Mock::expectFilterAdded('posts_distinct', [$this->posts, 'distinct'], $priority, $argsCount);
+        WP_Mock::expectFilterAdded('posts_join', [$this->posts, 'join'], $priority, $argsCount);
+        WP_Mock::expectFilterAdded('posts_search', [$this->posts, 'search'], $priority, $argsCount);
+
+        $this->posts->init();
+
+        WP_Mock::assertHooksAdded();
+    }
+}
