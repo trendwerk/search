3997d5e5259dcdc53a71e1db5719ebddf9d8f1bf
Harold Angenent <harold@trendwerk.nl>
2017-02-15 13:20:27 +0100

    ✅ Search: equals

--- tests/Search/Dimension/MetaTest.php
+++ tests/Search/Dimension/MetaTest.php
@@ -4,6 +4,7 @@ namespace Trendwerk\Search\Test;
 use BadMethodCallException;
 use Mockery;
 use Trendwerk\Search\Dimension\Meta;
+use WP_Mock;
 
 final class MetaTest extends TestCase
 {
@@ -20,5 +21,32 @@ final class MetaTest extends TestCase
     {
         $this->expectException(BadMethodCallException::class);
         $meta = new Meta($this->wpdb, []);
+    }
+
+    public function testSearchEquals()
+    {
+        $this->search('Testman', 'firstName', '=');
+    }
+
+    private function search($searchWord, $metaKey, $compare)
+    {
+        $expectation = "(searchMeta0.meta_key {$compare} %s AND searchMeta0.meta_value LIKE %s)";
+
+        WP_Mock::wpPassthruFunction('wp_parse_args', ['times' => 1]);
+
+        $meta = new Meta($this->wpdb, [
+            'compare' => $compare,
+            'key'     => $metaKey,
+        ]);
+
+        $this->wpdb->shouldReceive('prepare')
+            ->once()
+            ->andReturnUsing(function () {
+                return func_get_args();
+            });
+
+        $result = $meta->search($searchWord);
+
+        $this->assertEquals($result, [$expectation, $metaKey, $searchWord]);
     }
 }
