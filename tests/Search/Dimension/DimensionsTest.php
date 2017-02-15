a433fadf8376645f85a8ac76853d00a95624fa50
Harold Angenent <harold@trendwerk.nl>
2017-02-15 11:43:07 +0100

    ✅ Dimensions collection

--- tests/Search/Dimension/DimensionsTest.php
+++ tests/Search/Dimension/DimensionsTest.php
@@ -4,6 +4,7 @@ namespace Trendwerk\Search\Test;
 use Mockery;
 use Trendwerk\Search\Dimension\Dimensions;
 use Trendwerk\Search\Dimension\Meta;
+use WP_Mock;
 
 final class DimensionsTest extends TestCase
 {
@@ -18,10 +19,23 @@ final class DimensionsTest extends TestCase
 
     public function testGetSet()
     {
-        // $wpdb = Mockery::mock('\wpdb');
-        // $metaSearches = [
-        //     new Meta(),
-        //     new Meta()
-        // ];
+        $wpdb = Mockery::mock('wpdb');
+
+        WP_Mock::wpPassthruFunction('wp_parse_args', ['times' => 2]);
+
+        $metaSearches = [
+            new Meta($wpdb, [
+                'key' => 'firstName',
+            ]),
+            new Meta($wpdb, [
+                'key' => 'lastName',
+            ]),
+        ];
+
+        foreach ($metaSearches as $metaSearch) {
+            $this->assertNull($this->dimensions->add($metaSearch));
+        }
+
+        $this->assertEquals($this->dimensions->get(), $metaSearches);
     }
 }
