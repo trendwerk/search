db1ff3044894ea5388a0db775bcb8d6835711c89
Harold Angenent <harold@trendwerk.nl>
2017-02-15 11:46:32 +0100

    ✅ Meta: invalid construction

---
+++ tests/Search/Dimension/MetaTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Trendwerk\Search\Test;
+
+use BadMethodCallException;
+use Mockery;
+use Trendwerk\Search\Dimension\Meta;
+
+final class MetaTest extends TestCase
+{
+    private $wpdb;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->wpdb = Mockery::mock('wpdb');;
+    }
+
+    public function testKeyRequired()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $meta = new Meta($this->wpdb, []);
+    }
+}
