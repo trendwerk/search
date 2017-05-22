dad86b019e3f1d8bbdefb3874e6793488929742d
Harold Angenent <harold@trendwerk.nl>
2017-05-22 14:29:24 +0200

    :white_check_mark: Test term join

--- tests/Search/Dimension/TermTest.php
+++ tests/Search/Dimension/TermTest.php
@@ -22,4 +22,31 @@ final class TermTest extends TestCase
         $this->expectException(BadMethodCallException::class);
         $meta = new Term($this->wpdb, []);
     }
+
+    public function testJoin()
+    {
+        $tableAliasCount = 2;
+        $tableAlias = $this->tableAlias . $tableAliasCount;
+
+        $this->wpdb->posts = 'wp_posts';
+        $this->wpdb->term_relationships = 'wp_term_relationships';
+
+        $expectation = "INNER JOIN {$this->wpdb->term_relationships} AS {$tableAlias} ";
+        $expectation .= "ON ({$this->wpdb->posts}.ID = {$tableAlias}.object_id)";
+
+        $term = $this->create('taxonomyName');
+
+        $result = $term->join($tableAliasCount);
+
+        $this->assertEquals($expectation, $result);
+    }
+
+    private function create($taxonomy)
+    {
+        $term = new Term($this->wpdb, [
+            'taxonomy' => $taxonomy,
+        ]);
+
+        return $term;
+    }
 }
