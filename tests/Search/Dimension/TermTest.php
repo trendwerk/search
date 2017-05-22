d49cfc383109054257ff1e550017113493532359
Harold Angenent <harold@trendwerk.nl>
2017-05-22 15:08:58 +0200

    :white_check_mark: Test term search

--- tests/Search/Dimension/TermTest.php
+++ tests/Search/Dimension/TermTest.php
@@ -4,6 +4,7 @@ namespace Trendwerk\Search\Test;
 use BadMethodCallException;
 use Mockery;
 use Trendwerk\Search\Dimension\Term;
+use WP_Mock;
 
 final class TermTest extends TestCase
 {
@@ -15,6 +16,10 @@ final class TermTest extends TestCase
         parent::setUp();
 
         $this->wpdb = Mockery::mock('wpdb');
+        $this->wpdb->posts = 'wp_posts';
+        $this->wpdb->term_relationships = 'wp_term_relationships';
+        $this->wpdb->term_taxonomy = 'wp_term_taxonomy';
+        $this->wpdb->terms = 'wp_terms';
     }
 
     public function testKeyRequired()
@@ -28,15 +33,47 @@ final class TermTest extends TestCase
         $tableAliasCount = 2;
         $tableAlias = $this->tableAlias . $tableAliasCount;
 
-        $this->wpdb->posts = 'wp_posts';
-        $this->wpdb->term_relationships = 'wp_term_relationships';
-
         $expectation = "INNER JOIN {$this->wpdb->term_relationships} AS {$tableAlias} ";
         $expectation .= "ON ({$this->wpdb->posts}.ID = {$tableAlias}.object_id)";
 
         $term = $this->create('taxonomyName');
 
         $result = $term->join($tableAliasCount);
+
+        $this->assertEquals($expectation, $result);
+    }
+
+    public function testSearch()
+    {
+        $this->search('Testterm', 'testTaxonomy');
+    }
+
+    private function search($searchWord, $taxonomy, $tableAliasCount = 0)
+    {
+        $tableAlias = $this->tableAlias . $tableAliasCount;
+        $termIds = [18, 12];
+        $expectation = "{$tableAlias}.term_taxonomy_id IN (18,12)";
+
+        $term = $this->create($taxonomy);
+
+        WP_Mock::wpPassthruFunction('absint', ['times' => 2]);
+
+        $this->wpdb->shouldReceive('esc_like')
+            ->once()
+            ->with($searchWord)
+            ->andReturn($searchWord);
+
+        $this->wpdb->shouldReceive('prepare')
+            ->once()
+            ->andReturnUsing(function ($sql, $taxonomy, $search) {
+                return sprintf($sql, $taxonomy, $search);
+            });
+
+        $this->wpdb->shouldReceive('get_col')
+            ->once()
+            ->andReturn($termIds);
+
+        $result = $term->search($searchWord, $tableAliasCount);
 
         $this->assertEquals($expectation, $result);
     }
